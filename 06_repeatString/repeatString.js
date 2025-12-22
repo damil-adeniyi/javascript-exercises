e56d356afcdbef793655754e63a96715d3539d7a
Damil Adeniyi <damiladeniyi301@gmail.com>
2025-12-22 04:15:32 -0500

    subject: Create the repeatString function body: Create the repeatString() that takes a string and a number that repeats it the number of times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+function repeat(str, num) {
+    let string = "";
+    if (num < 0){
+        return "ERROR"
+    }else{
+        for (let i = 0; i < num; i++) {
+        let result = string += str;
+    }
+        }return string;
+}
+
+console.log(repeat("hey", 4));
+
+
