8983554b8361fb148b1d615b2024d1552998f794
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-02 09:31:20 -0500

    create the reverseString function that takes a string as input and returns the string reversed.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+let split = str.split('');
+let reverse = split.reverse();
+let join = reverse.join('');
 
+return join;
 };
+
+console.log(reverseString('hello'));
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
