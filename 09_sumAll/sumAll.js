393e2ae765fff77403209430ea4b967297bfdc37
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-03 03:49:57 -0500

    create a sumAll function that takes a range and outputs the sum of all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let newArray = [];
+for(let i = start; i <= end; i++){
+    newArray.push(i);
+    
+}
+let result = newArray.reduce((sum, current) => sum + current, 0)
+
+return result;
 
 };
+
+console.log(sumAll(1, 4));
+
 
 // Do not edit below this line
 module.exports = sumAll;
