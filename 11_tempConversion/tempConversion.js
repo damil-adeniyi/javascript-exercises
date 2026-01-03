84ffd543c6e604d197be69aaabfe4b1971d09fb8
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-03 10:06:34 -0500

    create two functions that conver fahrenheit to celsius and celsius to fahrenheit and outputs them in a decimal place

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let conversion = (value - 32) * 5/9;
+  let round = Math.round(conversion * 10) /10;
+
+  return round;
+  
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(32));
+
+
+const convertToFahrenheit = function(value) {
+  let conversion = (value * 9/5 + 32) ;
+  let round = Math.round(conversion * 10) /10;
+
+  return round;
 };
+
+console.log(convertToFahrenheit(0));
 
 // Do not edit below this line
 module.exports = {
