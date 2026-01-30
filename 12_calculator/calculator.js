401c2e455c7bd998449c133c3b62262ea9876779
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-30 09:11:11 -0500

    create different functions: add(): take two numbers and return their sum subtract(): take two numbers and return the result after subtracting the second from the first sum(): take an array of numbers and add all the numbers together multiply(): take an array of numbers and multiply all the numbers together power(): take two numbers (base and exponent) and return the result of raising the base to that exponent factorial(): given a single number, find its factorial

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sumAll = arr.reduce((acc, intial) => {
+    return acc + intial;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sumAll = arr.reduce((acc, intial) => {
+    return acc * intial;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+
+const factorial = function(num) {
+	if(num === 0){
+    return 1
+  }else{
+    let arr = [];
+    for(let i = 1; i <= num; i++){
+        arr.push(i);
+    } 
+    
+    let result = arr.reduce((acc, initial)=> {
+      return acc * initial;
+    }, 1);
+
+    return result
+  }
 };
 
 // Do not edit below this line
