e189e6980f9e187cdcd31596bfa33259019bf640
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-30 11:14:36 -0500

    solve the palindromes function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // original
+  const sense = string.toLowerCase().replaceAll(" ", "").replaceAll(",", "").replaceAll(".", "");
+  let split = sense.split("")
+  let newSplit = split.join("|")
+
+  // reversed
+  let reverse = split.slice().reverse();
+  let newReverse = reverse.join("|")
+
+  // condition
+  if(newSplit === newReverse){
+    return true;
+  } else {
+    return false
+  }
 
 };
 
