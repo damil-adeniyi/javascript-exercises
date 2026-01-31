3c44a3863973cc7cae623af7524f81f616bf527c
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-31 08:26:34 -0500

    Solve the findTheOldest function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+ const findTheOldest = (arr) => {
 
-};
+      // Check for max age
+        let emptyArr = [];
+        let age = arr.map((person) => {
+
+          if(person.yearOfDeath === undefined){
+            const currentYear = new Date().getFullYear();
+            return currentYear - person.yearOfBirth
+          }else{return person.yearOfDeath - person.yearOfBirth;}
+          
+           })
+        emptyArr.push(age);
+        let max = Math.max(...age);
+
+      //  prints out object     
+        let oldestPerson = arr.find(person => {
+      // We must recalculate age logic to compare it to max
+          const death = person.yearOfDeath || new Date().getFullYear();
+          const personAge = death - person.yearOfBirth;
+    
+          return personAge === max; // Returns the first object that matches
+         });
+
+      return oldestPerson;
+    }
 
 // Do not edit below this line
 module.exports = findTheOldest;
