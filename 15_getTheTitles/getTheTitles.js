c61c3c08c5bc6e728def99ad7d65e810f8414af8
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-01-31 05:46:44 -0500

    solve the getTheTitle function

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+  let empty = [];
+  
+  let title = arr.map((book) => {
+    return book.title;
+    
+  })
+
+  empty.push(title);
+
+  return (title);
+  
 
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
