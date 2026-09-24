5dd298d2e737548a6ccb7e8fc0139139e7bb70f4
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-09-24 06:42:19 -0500

    modify factorial function test factorial function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,17 +1,16 @@
 const { log } = require('node:console');
 
 const factorial = function (int) {
-	if (typeof int == 'string') {
-		return;
-	} else {
-		if (int === 0) {
+	if (typeof int == 'string' || int % 1 !== 0 || int < 0) {
+		return ;
+	} if (int === 0) {
 			return 1;
 		}
-		return int * factorial(int - 1);
-	}
+	return int * factorial(int - 1);
+	
 };
 
-console.log(factorial(5));
+console.log(factorial(-4));
 
 // Do not edit below this line
 module.exports = factorial;
