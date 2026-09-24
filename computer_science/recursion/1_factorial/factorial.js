94be806639637488e7b21d7ce3734d423ddd246e
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-09-24 05:46:55 -0500

    install and setup dependencies for the project add factoral function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,17 @@
-const factorial = function() {
+const { log } = require('node:console');
 
+const factorial = function (int) {
+	if (typeof int == 'string') {
+		return;
+	} else {
+		if (int === 0) {
+			return 1;
+		}
+		return int * factorial(int - 1);
+	}
 };
+
+console.log(factorial(5));
 
 // Do not edit below this line
 module.exports = factorial;
