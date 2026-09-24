84c3f9ff68fb217250a2d112eb3cbb97f959bcee
Damil Adeniyi <damiladeniyi301@gmail.com>
2026-09-24 06:54:37 -0500

    add contain function and test

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,24 @@
-const contains = function() {
-  
+const contains = function (obj, targetValue) {
+	// Base case: if the current value strictly matches the target value, return true
+	if (obj === targetValue) {
+		return true;
+	}
+
+	// If obj is an object (and not null), iterate through its properties recursively
+	if (typeof obj === 'object' && obj !== null) {
+		for (const key in obj) {
+			if (contains(obj[key], targetValue)) {
+				return true;
+			}
+		}
+	}
+
+	// Return false if the value is not found in this subtree
+	return false;
 };
-  
+
+console.log(contains({ foo: { bar: "bar" } }, "bar"));
+
+
 // Do not edit below this line
 module.exports = contains;
